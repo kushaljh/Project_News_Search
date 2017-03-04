55a354dd6eb612fd319dc570ec499c0172faddcf
Helly Shah <hellys@uw.edu>
2017-03-03 22:59:05 -0800

    add css

--- ui.R
+++ ui.R
@@ -10,13 +10,19 @@ ui <- fluidPage(
                   choices = list("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
                                  "June" = 6, "July" = 7, "August" = 8, "September" = 9, "October" = 10,
                                  "November" = 11, "December" = 12), selected = 1),
-      selectInput("day", label = h3("Select Day"), choices = as.list(seq(1, 31, by = 1)), selected = 1),
+      selectInput("day", label = h3("Select Day"), choices = sprintf("%02d", c(seq(01,31, by = 1))), selected = 1),
       selectInput("section", label = h3("Select Section"), choices = as.list("World", "U.S.", "Politics", "Business", "Opinion",
                                                                              "Tech", "Science", "Health", "Sports", "Arts",
                                                                              "Style", "Food", "Travel"), selected = 3)
     ),
     
     mainPanel(
+      tabsetPanel(
+        tabPanel("TABLE", 
+                 h3("Articles"),
+                 br(),
+                 dataTableOutput("table"))
+      )
     )
   )
 )
