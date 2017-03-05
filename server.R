6c09ab0a1aadb9fa86a7d77c9d794d41ef143e03
Manesh Jhawar <manjhawar96@gmail.com>
2017-03-04 20:04:12 -0800

    add new server functions

--- server.R
+++ server.R
@@ -3,8 +3,9 @@ library("httr")
 library("jsonlite")
 library("dplyr")
 library("stringr")
+library("DT")
+library("plotrix")
 
-#my.server <- 
 shinyServer(
   function(input, output) {
     
@@ -25,28 +26,104 @@ shinyServer(
        return(data)
     }
          
-    
-    output$table <- renderDataTable({
+    get.news.data <- function() {
       
       parameters <- input.data()
-      
       news.data <- get.uri.data(parameters[1], parameters[2])
       news.data.response <- news.data$response
       news.data.response.docs <- news.data.response$docs
       news.data.response.docs.headline <- news.data.response.docs$headline
       news.data.response.docs.keywords <- news.data.response.docs$keywords
       news.data.table <- data.frame(news.data.response.docs$pub_date,
-                              news.data.response.docs.headline$main, news.data.response.docs$section_name)
+                                    news.data.response.docs.headline$main, news.data.response.docs$section_name)
       colnames(news.data.table) <- c("Publication Date", "Headline", "Section")
-      View(news.data.table)
       news.data.table$Headline <- paste0("<a href='",news.data.response.docs$web_url,"'>",news.data.table$Headline,"</a>")
       news.data.table$Section <- str_replace_na(news.data.table$Section, replacement = "Section not defined")
       news.data.table$`Publication Date` <- str_sub(news.data.table$`Publication Date`, 1, 10)
       news.data.table$Day <- str_sub(news.data.table$`Publication Date`, 9, 10)
-      news.data.table.final <- filter(news.data.table, news.data.table$Day == parameters[3])
+      return(news.data.table)
+    }
+   
+     output$table <- renderDataTable({
       
-      return(news.data.table.final)
+      parameters <- input.data()
+      table.final <- get.news.data()
+      table.final <- filter(table.final, table.final$Day == parameters[3])
+      table.final <- datatable(table.final, escape = FALSE)
+      return(table.final)
+      
     })
-    
+     
+     output$plot1 <- renderPlotly({
+      
+       data.plot <- get.news.data()
+       data.plot <- group_by(data.plot, Section)
+       data.plot2 <- summarise(data.plot, 
+                               Count = n()) %>% 
+         arrange(-Count)
+       
+       plot <- ggplot(data = data.plot) +
+         geom_bar(mapping = aes(x = Section), fill = rainbow(n = length(data.plot2$Count)))+
+         labs(title = "Section Trend, Monthly Basis",
+              y = "Count",
+              x  = "Section") +
+         theme_light() + 
+         theme(
+           axis.text.x = element_blank(),
+           axis.text.y = element_blank(),
+           axis.ticks = element_blank(),
+           panel.grid.minor = element_blank(), 
+           panel.grid.major = element_blank(),
+           panel.background = element_rect(fill=NA),
+           plot.background = element_rect(fill=NA))
+         
+       plot <- ggplotly(plot)
+       return(plot)
+     })
+     
+     output$plot2 <- renderPlotly({
+       
+       parameters <- input.data()
+       data.plot <- get.news.data()
+       data.plot <- filter(data.plot, data.plot$Day == parameters[3]) %>% 
+         group_by(Section)
+       data.plot2 <- summarise(data.plot, 
+                               Count = n()) %>% 
+         arrange(-Count)
+       
+       plot <- ggplot(data = data.plot) +
+         geom_bar(mapping = aes(x = Section), fill = rainbow(n = length(data.plot2$Count))) +
+         labs(title = "Section Trend, Daily Basis",
+              y = "Count",
+              x  = "Section") + 
+         theme_minimal() +
+         theme(
+           axis.text.x = element_blank(),
+           axis.text.y = element_blank(),
+           axis.ticks = element_blank(),
+           panel.grid.minor = element_blank(), 
+           panel.grid.major = element_blank(),
+           panel.background = element_rect(fill=NA),
+           plot.background = element_rect(fill=NA)) 
+         
+       plot <- ggplotly(plot)
+       return(plot)
+     })
+     
+     output$plot3 <- renderPlot({
+       
+       parameters <- input.data()
+       data.plot <- get.news.data()
+       data.plot <- filter(data.plot, data.plot$Day == parameters[3]) %>% 
+         group_by(Section)
+       data.plot2 <- summarise(data.plot, 
+                               Count = n()) %>% 
+         arrange(-Count)
+       
+       top.10 <- data.plot2[1:10, ]
+       plot <- pie3D(top.10$Count,labels = top.10$Section ,explode=0.2,
+             main="Daily top 10")
+       return(plot)
+     })
 })
 
--- ui.R
+++ ui.R
@@ -1,30 +1,46 @@
 library("shiny")
+library("plotly")
+library("plotrix")
 
-ui <- fluidPage(
-  theme = "bootstrap.css",
-  titlePanel(""),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("year", label = h3("Select Year"), choices = as.list(seq(1852, 2017, by = 1)), selected = 1),
-      selectInput("month", label = h3("Select Month"), 
-                  choices = list("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
-                                 "June" = 6, "July" = 7, "August" = 8, "September" = 9, "October" = 10,
-                                 "November" = 11, "December" = 12), selected = 1),
-      selectInput("day", label = h3("Select Day"), choices = sprintf("%02d", c(seq(01,31, by = 1))), selected = 1),
-      selectInput("section", label = h3("Select Section"), choices = as.list("World", "U.S.", "Politics", "Business", "Opinion",
-                                                                             "Tech", "Science", "Health", "Sports", "Arts",
-                                                                             "Style", "Food", "Travel"), selected = 3)
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-        tabPanel("TABLE", 
-                 h3("Articles"),
-                 br(),
-                 dataTableOutput("table"))
+shinyUI(
+  fluidPage(
+    theme = "bootstrap.css",
+    titlePanel(""),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("year", label = h3("Select Year"), choices = as.list(seq(1852, 2017, by = 1)), selected = 2000),
+        selectInput("month", label = h3("Select Month"), 
+                    choices = list("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+                                   "June" = 6, "July" = 7, "August" = 8, "September" = 9, "October" = 10,
+                                   "November" = 11, "December" = 12), selected = 1),
+        selectInput("day", label = h3("Select Day"), choices = sprintf("%02d", c(seq(01,31, by = 1))), selected = 1),
+        selectInput("section", label = h3("Select Section"), choices = as.list("World", "U.S.", "Politics", "Business", "Opinion",
+                                                                               "Tech", "Science", "Health", "Sports", "Arts",
+                                                                               "Style", "Food", "Travel"), selected = 3)
+      ),
+      
+      mainPanel(
+        tabsetPanel(
+          tabPanel("TABLE", 
+                   h3("Articles"),
+                   br(),
+                   dataTableOutput("table")),
+        
+         tabPanel("Visualization",
+                  tabsetPanel(
+                    tabPanel("Monthly",
+                             br(),
+                             plotlyOutput("plot1")),
+                    tabPanel("Daily",
+                             br(),
+                             plotlyOutput("plot2")),
+                    tabPanel("Top 10 Daily",
+                              br(),
+                              plotOutput("plot3")))
+          )
+        )
       )
     )
   )
 )
 
-shinyUI(ui)
