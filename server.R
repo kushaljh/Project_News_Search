c346880d58da8eac86d8bb108b86207f5ac54e17
Kushal Jhunjhunwalla <kushal.jhunjhunwalla@gmail.com>
2017-03-05 03:15:07 -0800

    add section graphical visualizations and edit bootstrap file for better visualization of summary table

--- server.R
+++ server.R
@@ -1,10 +1,8 @@
-#install.packages("DT")
 library("shiny")
 library("httr")
 library("jsonlite")
 library("dplyr")
 library("stringr")
-
 library("DT")
 library("plotrix")
 
@@ -19,7 +17,8 @@ shinyServer(
       year.in <- input$year
       month.in <- input$month
       day.in <- input$day
-      return(c(year.in, month.in, day.in))
+      section.in <- input$section
+      return(c(year.in, month.in, day.in, section.in))
     })
 
     get.uri.data <- function(year, month) {
@@ -50,8 +49,8 @@ shinyServer(
       
       parameters <- input.data()
       table.final <- get.news.data()
-      table.final <- filter(table.final, table.final$Day == parameters[3])
-      table.final <- datatable(table.final, escape = FALSE)
+      table.final <- filter(table.final, table.final$Day == parameters[3]) %>% select(-`Day`)
+      table.final <- datatable(table.final, escape = FALSE, class = "hover")#, style = "bootstrap"
       return(table.final)
       
     })
@@ -126,6 +125,52 @@ shinyServer(
        plot <- pie3D(top.10$Count,labels = top.10$Section ,explode=0.2,
              main="Daily top 10")
        return(plot)
+     })
+     
+     output$plot.trend <- renderPlotly({
+       parameters <- input.data()
+       news.data <- get.uri.data(parameters[1], parameters[2])
+       
+       plot.data <- get.news.data() %>%
+         select(`Section`, `Day`) %>%
+         filter(Section == parameters[4]) %>%
+         group_by(Day) %>%
+         summarize(`No of Articles` = n())
+       
+       p <- ggplot(data = plot.data) +
+         geom_point(mapping = aes(x = Day, y = `No of Articles`),
+                    color = "blue",
+                    alpha=0.4, size = plot.data$`No of Articles`) + 
+         labs(x = "Day of Month", 
+              y = "Number of Articles") +
+         theme_minimal() +
+         theme(
+           axis.text.x = element_blank(),
+           axis.text.y = element_blank(),
+           axis.ticks = element_blank(),
+           panel.grid.minor = element_blank(), 
+           panel.grid.major = element_blank(),
+           panel.background = element_rect(fill=NA),
+           plot.background = element_rect(fill=NA))
+       
+       ggplotly(p)
+     })
+     
+     output$plot.trendline <- renderPlotly({
+       parameters <- input.data()
+       news.data <- get.uri.data(parameters[1], parameters[2])
+       
+       plot.data <- get.news.data() %>%
+         select(`Section`, `Day`) %>%
+         filter(Section == parameters[4]) %>%
+         group_by(Day) %>%
+         summarize(`No of Articles` = n())
+       
+       plot_ly(plot.data, x = ~plot.data$Day, y = ~plot.data$`No of Articles`,
+               type = 'scatter', mode = 'lines') %>% 
+         layout(title = paste0("Monthwise Trend of ", parameters[4]),
+                xaxis = list(title = "Day of Month"), 
+                yaxis = list(title = "Number of Articles"))
      })
 })
 
--- ui.R
+++ ui.R
@@ -1,5 +1,3 @@
-#install.packages("plotrix")
-#install.packages("plotly")
 library("shiny")
 library("plotly")
 library("plotrix")
@@ -10,15 +8,23 @@ shinyUI(
     titlePanel(""),
     sidebarLayout(
       sidebarPanel(
-        selectInput("year", label = h3("Select Year"), choices = as.list(seq(1852, 2017, by = 1)), selected = 2000),
+        selectInput("year", label = h3("Select Year"), choices = as.list(seq(1852, 2017, by = 1)),
+                    selected = 2017),
+        
         selectInput("month", label = h3("Select Month"), 
-                    choices = list("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
-                                   "June" = 6, "July" = 7, "August" = 8, "September" = 9, "October" = 10,
+                    choices = list("January" = 1, "February" = 2, 
+                                   "March" = 3, "April" = 4, "May" = 5,
+                                   "June" = 6, "July" = 7, "August" = 8,
+                                   "September" = 9, "October" = 10,
                                    "November" = 11, "December" = 12), selected = 1),
-        selectInput("day", label = h3("Select Day"), choices = sprintf("%02d", c(seq(01,31, by = 1))), selected = 1),
-        selectInput("section", label = h3("Select Section"), choices = as.list("World", "U.S.", "Politics", "Business", "Opinion",
-                                                                               "Tech", "Science", "Health", "Sports", "Arts",
-                                                                               "Style", "Food", "Travel"), selected = 3)
+        
+        selectInput("day", label = h3("Select Day"), 
+                    choices = sprintf("%02d", c(seq(01,31, by = 1))), selected = 1),
+        
+        selectInput("section", label = h3("Select Section"), 
+                    choices = as.list("World", "U.S.", "Politics", "Business", "Opinion",
+                                      "Tech", "Science", "Health", "Sports", "Arts",
+                                      "Style", "Food", "Travel"), selected = 3)
       ),
       
       mainPanel(
@@ -27,18 +33,24 @@ shinyUI(
                    h3("Articles"),
                    br(),
                    dataTableOutput("table")),
-        
-         tabPanel("Visualization",
-                  tabsetPanel(
-                    tabPanel("Monthly",
-                             br(),
-                             plotlyOutput("plot1")),
-                    tabPanel("Daily",
-                             br(),
-                             plotlyOutput("plot2")),
-                    tabPanel("Top 10 Daily",
+          
+          tabPanel("Visualization",
+                   tabsetPanel(
+                     tabPanel("Monthly",
                               br(),
-                              plotOutput("plot3")))
+                              plotlyOutput("plot1")),
+                     tabPanel("Daily",
+                              br(),
+                              plotlyOutput("plot2")),
+                     tabPanel("Top 10 Daily",
+                              br(),
+                              plotOutput("plot3")), 
+                     tabPanel("Section Trend",
+                              br(),
+                              plotlyOutput("plot.trend")),
+                     tabPanel("Section Trend Line",
+                              br(),
+                              plotlyOutput("plot.trendline")))
           )
         )
       )
