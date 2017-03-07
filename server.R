2ba2ed966eb1fa17b469246f5d380da9965164ee
Manesh Jhawar <manjhawar96@gmail.com>
2017-03-07 14:07:02 -0800

    Many changes

--- server.R
+++ server.R
@@ -4,37 +4,55 @@ library("jsonlite")
 library("dplyr")
 library("stringr")
 library("DT")
-library("plotrix")
 library("tidyr")
 library("ggplot2")
 library("plotly")
 
 shinyServer(
   function(input, output) {
-    
     base.uri.1 <- "http://api.nytimes.com/svc/archive/v1/"
     base.uri.2 <- ".json?api-key="
     api.key <- "3d4af681d88646f5afb0b40f2a1e510b"
-  
-    input.data <- reactive({
-      year.in <- input$year
-      month.in <- input$month
-      day.in <- input$day
-      section1.in <- input$section.1
-      section2.in <- input$section.2
-      return(c(year.in, month.in, day.in, section1.in, section2.in))
+    
+    input.article <- reactive({
+      year.article <- input$year.article
+      month.article <- input$month.article
+      day.article <- input$day.article
+      return(c(year.article, month.article, day.article))
     })
-
+    
+    input.viz <- reactive({
+      year.viz <- input$year.viz
+      month.viz <- input$month.viz
+      day.viz <- input$day.viz
+      section.viz <- input$section.viz
+      return(c(year.viz, month.viz, day.viz, section.viz))
+    })
+    
+    input.pop <- reactive({
+      year.pop <- input$year.pop
+      month.pop<- input$month.pop
+      section1.pop <- input$section1.pop
+      section2.pop <- input$section2.pop
+      return(c(year.pop, month.pop, section1.pop, section2.pop))
+    })
+    
+    input.top <- reactive({
+      year.top <- input$year.top
+      month.top<- input$month.top
+      return(c(year.top, month.top))
+    })
+    
     get.uri.data <- function(year, month) {
-       uri <- paste0(base.uri.1, year, "/" ,month, base.uri.2, api.key)   
-       data <- fromJSON(content(GET(uri), "text"))
-       return(data)
+      uri <- paste0(base.uri.1, year, "/" ,month, base.uri.2, api.key)   
+      data <- fromJSON(content(GET(uri), "text"))
+      print(uri)
+      return(data)
     }
-         
-    get.news.data <- function() {
+    
+    get.news.data <- function(pass.year,pass.month) {
       
-      parameters <- input.data()
-      news.data <- get.uri.data(parameters[1], parameters[2])
+      news.data <- get.uri.data(pass.year, pass.month)
       news.data.response <- news.data$response
       news.data.response.docs <- news.data.response$docs
       news.data.response.docs.headline <- news.data.response.docs$headline
@@ -51,7 +69,7 @@ shinyServer(
     
     get.values.data <- function() {
       
-      parameters <- input.data()
+      parameters <- input.top()
       data <- as.data.frame(get.uri.data(parameters[1], parameters[2]))
       news.data.table <-  data %>% 
         select(response.docs.keywords)
@@ -61,7 +79,7 @@ shinyServer(
         return(keywords$value)
       }
       
-      values <- GetValues(1)
+      values <- GetValues()
       
       for (index in 2:nrow(news.data.table)) {
         values <- c(values, GetValues(index))
@@ -76,166 +94,156 @@ shinyServer(
       values <- values[1:10, ]
       return (values)
     }
-   
-     output$table <- renderDataTable({
+    
+    output$table <- renderDataTable({
       
-      parameters <- input.data()
-      table.final <- get.news.data()
+      parameters <- input.article()
+      print(parameters)
+      table.final <- get.news.data(parameters[1], parameters[2])
       table.final <- filter(table.final, table.final$Day == parameters[3]) %>% select(-`Day`)
       table.final <- datatable(table.final, escape = FALSE, class = "hover")
       return(table.final)
       
     })
-     
-     output$plot1 <- renderPlotly({
+    
+    output$plot1 <- renderPlotly({
       
-       data.plot <- get.news.data()
-       data.plot <- group_by(data.plot, Section)
-       data.plot2 <- summarise(data.plot, 
-                               Count = n()) %>% 
-         arrange(-Count)
-       
-       plot <- ggplot(data = data.plot) +
-         geom_bar(mapping = aes(x = Section), fill = rainbow(n = length(data.plot2$Count)))+
-         labs(title = "Section Trend, Monthly Basis",
-              y = "Count",
-              x  = "Section") +
-         theme_light() + 
-         theme(
-           axis.text.x = element_blank(),
-           axis.text.y = element_blank(),
-           axis.ticks = element_blank(),
-           panel.grid.minor = element_blank(), 
-           panel.grid.major = element_blank(),
-           panel.background = element_rect(fill=NA),
-           plot.background = element_rect(fill=NA))
-         
-       plot <- ggplotly(plot)
-       return(plot)
-     })
-     
-     output$plot2 <- renderPlotly({
-       
-       parameters <- input.data()
-       data.plot <- get.news.data()
-       data.plot <- filter(data.plot, data.plot$Day == parameters[3]) %>% 
-         group_by(Section)
-       data.plot2 <- summarise(data.plot, 
-                               Count = n()) %>% 
-         arrange(-Count)
-       
-       plot <- ggplot(data = data.plot) +
-         geom_bar(mapping = aes(x = Section), fill = rainbow(n = length(data.plot2$Count))) +
-         labs(title = "Section Trend, Daily Basis",
-              y = "Count",
-              x  = "Section") + 
-         theme_minimal() +
-         theme(
-           axis.text.x = element_blank(),
-           axis.text.y = element_blank(),
-           axis.ticks = element_blank(),
-           panel.grid.minor = element_blank(), 
-           panel.grid.major = element_blank(),
-           panel.background = element_rect(fill=NA),
-           plot.background = element_rect(fill=NA)) 
-         
-       plot <- ggplotly(plot)
-       return(plot)
-     })
-     
-     output$plot3 <- renderPlotly({
-       
-       parameters <- input.data()
-       data.plot <- get.news.data()
-       data.plot <- filter(data.plot, data.plot$Day == parameters[3]) %>% 
-         group_by(Section)
-       data.plot2 <- summarise(data.plot, 
-                               Count = n()) %>% 
-         arrange(-Count)
-       
-       top.10 <- data.plot2[1:10, ] %>% 
-       plot_ly(labels = ~Section, values = ~Count) %>% 
-         add_pie(hole = 0.5) %>% 
-         layout(title = "TOP 10 SECTIONS, DAILY BASIS",  showlegend = F,
-                xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-                yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
-       top.10
-       
-     })
-     
-     output$plot.trend <- renderPlotly({
-       parameters <- input.data()
-       news.data <- get.uri.data(parameters[1], parameters[2])
-       
-       plot.data <- get.news.data() %>%
-         select(`Section`, `Day`) %>%
-         filter(Section == parameters[4]) %>%
-         group_by(Day) %>%
-         summarize(`No of Articles` = n())
-       
-       p <- ggplot(data = plot.data) +
-         geom_point(mapping = aes(x = Day, y = `No of Articles`),
-                    color = "blue",
-                    alpha=0.4, size = plot.data$`No of Articles`) + 
-         labs(x = "Day of Month", 
-              y = "Number of Articles") +
-         theme_minimal() +
-         theme(
-           axis.text.x = element_blank(),
-           axis.text.y = element_blank(),
-           axis.ticks = element_blank(),
-           panel.grid.minor = element_blank(), 
-           panel.grid.major = element_blank(),
-           panel.background = element_rect(fill=NA),
-           plot.background = element_rect(fill=NA))
-       
-       ggplotly(p)
-     })
-     
-     output$plot.trendline <- renderPlotly({
-       parameters <- input.data()
-       news.data <- get.uri.data(parameters[1], parameters[2])
-       
-       GetData <- function(index) {
-         plot.data <- get.news.data() %>%
-           select(`Section`, `Day`) %>%
-           filter(Section == parameters[index]) %>%
-           group_by(Day) %>%
-           summarize(`No of Articles` = n())
-         colnames(plot.data)[2] <- parameters[index]
-         return(plot.data)
-       }
-       
-       plot.data.1 <- GetData(4)
-       plot.data.2 <- GetData(5)
-       plot.final <- full_join(plot.data.1, plot.data.2)
-       
-       traces <- parameters[4:5]
-       
-       plot_ly(plot.final, x = ~plot.final[[1]], y = ~plot.final[[2]],
-               type = 'scatter', mode = 'lines', name = traces[1]) %>% 
-         add_trace(x = ~plot.final[[1]], y = ~plot.final[[3]], name = traces[2]) %>% 
-         layout(title = paste("Popularity of", traces[1], "vs", traces[2]),
-                xaxis = list(title = "Day of Month"), 
-                yaxis = list(title = "Number of Articles"))
-     })
-     
-     output$plot.values <- renderPlotly ({
-       values <- get.values.data()
-       JitCoOr <- jitter(as.numeric(factor(values$Values)))
-       JitCoOr2 <- jitter(as.numeric(factor(values$Frequency)))
-       
-       plot <- ggplot(data, aes(x = Frequency, y = Values)) +
-         geom_point(data = values, aes(x = JitCoOr2, y = JitCoOr, size = Frequency, color = Values), alpha = .5) +
-         geom_text(data = values, aes(x = JitCoOr2, y = JitCoOr, label = Values)) +
-         scale_size(range = c(5, 20)) +
-         scale_x_discrete(breaks = 1:7, labels = c("Low", " ", " ", "Medium", " ", " ", "High"), limits = 1:7) +
-         scale_y_discrete(breaks = 1:10 , labels = as.vector(arrange(values, JitCoOr)[, 1]), limits = 1:10) + 
-         theme_bw() +
-         labs(title = "Top 10 most-discussed about topics in New York Times", x = "Distribution according to recurrence",
-              y = "Topics of Interest", color = "", size = "")
-       
-       ggplotly(plot) %>% layout(plot, showlegend = FALSE)
-     })
-})
+      parameters <- input.viz()
+      data.plot <- get.news.data(parameters[1], parameters[2])
+      data.plot <- group_by(data.plot, Section)
+      data.plot2 <- summarise(data.plot, 
+                              Count = n()) %>% 
+        arrange(-Count)
+      
+      plot <- ggplot(data = data.plot) +
+        geom_bar(mapping = aes(x = Section), fill = rainbow(n = length(data.plot2$Count)))+
+        labs(title = "Section Trend, Monthly Basis",
+             y = "Count",
+             x  = "Section") +
+        theme_light() + 
+        theme(
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid.minor = element_blank(), 
+          panel.grid.major = element_blank())
+      
+      plot <- ggplotly(plot)
+      return(plot)
+    })
+    
+    output$plot2 <- renderPlotly({
+      
+      parameters <- input.viz()
+      data.plot <- get.news.data(parameters[1], parameters[2])
+      data.plot <- filter(data.plot, data.plot$Day == parameters[3]) %>% 
+        group_by(Section)
+      data.plot2 <- summarise(data.plot, 
+                              Count = n()) %>% 
+        arrange(-Count)
+      
+      plot <- ggplot(data = data.plot) +
+        geom_bar(mapping = aes(x = Section), fill = rainbow(n = length(data.plot2$Count))) +
+        labs(title = "Section Trend, Daily Basis",
+             y = "Count",
+             x  = "Section") + 
+        theme_minimal() +
+        theme(
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid.minor = element_blank(), 
+          panel.grid.major = element_blank())
+      
+      plot <- ggplotly(plot)
+      return(plot)
+    })
+    
+    output$plot3 <- renderPlotly({
+      
+      parameters <- input.viz()
+      data.plot <- get.news.data(parameters[1], parameters[2])
+      data.plot <- filter(data.plot, data.plot$Day == parameters[3]) %>% 
+        group_by(Section)
+      data.plot2 <- summarise(data.plot, 
+                              Count = n()) %>% 
+        arrange(-Count)
+      
+      top.10 <- data.plot2[1:10, ] %>% 
+        plot_ly(labels = ~Section, values = ~Count) %>% 
+        add_pie(hole = 0.5) %>% 
+        layout(title = "TOP 10 SECTIONS, DAILY BASIS",  showlegend = F,
+               xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+               yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+      top.10
+      
+    })
+    
+    output$plot.trend <- renderPlotly({
+      parameters <- input.viz()
+      plot.data <- get.news.data(parameters[1], parameters[2]) %>%
+        select(`Section`, `Day`) %>%
+        filter(Section == parameters[4]) %>%
+        group_by(Day) %>%
+        summarize(`No of Articles` = n())
+      
+      p <- ggplot(data = plot.data) +
+        geom_point(mapping = aes(x = Day, y = `No of Articles`),
+                   color = "blue",
+                   alpha=0.4, size = plot.data$`No of Articles`) + 
+        labs(x = "Day of Month", 
+             y = "Number of Articles") +
+        theme_minimal() +
+        theme(
+          axis.ticks = element_blank(),
+          panel.grid.minor = element_blank(), 
+          panel.grid.major = element_blank())
+      ggplotly(p)
+    })
+    
+    output$plot.trendline <- renderPlotly({
+      parameters <- input.pop()
+      
+      GetData <- function(index) {
+        plot.data <- get.news.data(parameters[1], parameters[2]) %>%
+          select(`Section`, `Day`) %>%
+          filter(Section == parameters[index]) %>%
+          group_by(Day) %>%
+          summarize(`No of Articles` = n())
+        colnames(plot.data)[2] <- parameters[index]
+        return(plot.data)
+      }
+      
+      plot.data.1 <- GetData(3)
+      plot.data.2 <- GetData(4)
+      plot.final <- full_join(plot.data.1, plot.data.2)
+      
+      traces <- parameters[3:4]
+      
+      plot_ly(plot.final, x = ~plot.final[[1]], y = ~plot.final[[2]],
+              type = 'scatter', mode = 'lines', name = traces[1]) %>% 
+        add_trace(x = ~plot.final[[1]], y = ~plot.final[[3]], name = traces[2]) %>% 
+        layout(title = paste("Popularity of", traces[1], "vs", traces[2]),
+               xaxis = list(title = "Day of Month"), 
+               yaxis = list(title = "Number of Articles"))
+    })
+    
+    output$plot.values <- renderPlotly ({
+      values <- get.values.data()
+      JitCoOr <- jitter(as.numeric(factor(values$Values)))
+      JitCoOr2 <- jitter(as.numeric(factor(values$Frequency)))
+      
+      plot <- ggplot(data, aes(x = Frequency, y = Values)) +
+        geom_point(data = values, aes(x = JitCoOr2, y = JitCoOr, size = Frequency, color = Values), alpha = .5) +
+        geom_text(data = values, aes(x = JitCoOr2, y = JitCoOr, label = Values)) +
+        scale_size(range = c(5, 20)) +
+        scale_x_discrete(breaks = 1:7, labels = c("Low", " ", " ", "Medium", " ", " ", "High"), limits = 1:7) +
+        scale_y_discrete(breaks = 1:10 , labels = as.vector(arrange(values, JitCoOr)[, 1]), limits = 1:10) + 
+        theme_bw() +
+        labs(title = "Top 10 most-discussed about topics in New York Times", x = "Distribution according to recurrence",
+             y = "Topics of Interest", color = "", size = "")
+      
+      ggplotly(plot) %>% layout(plot, showlegend = FALSE)
+    })
+  })
 
--- ui.R
+++ ui.R
@@ -1,69 +1,97 @@
 library("shiny")
 library("plotly")
-library("plotrix")
 library("DT")
-
-shinyUI(
-  fluidPage(
-    theme = "bootstrap.css",
-    titlePanel(""),
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("year", label = h3("Select Year"), choices = as.list(seq(1852, 2017, by = 1)),
-                    selected = 2017),
-        
-        selectInput("month", label = h3("Select Month"), 
-                    choices = list("January" = 1, "February" = 2, 
-                                   "March" = 3, "April" = 4, "May" = 5,
-                                   "June" = 6, "July" = 7, "August" = 8,
-                                   "September" = 9, "October" = 10,
-                                   "November" = 11, "December" = 12), selected = 1),
-        
-        selectInput("day", label = h3("Select Day"), 
-                    choices = sprintf("%02d", c(seq(01,31, by = 1))), selected = 1),
-        
-        selectInput("section.1", label = h3("Select Section"), 
-                    choices = as.list(c("World", "U.S.", "Politics", "Business", "Opinion",
-                                      "Tech", "Science", "Health", "Sports", "Arts",
-                                      "Style", "Food", "Travel")), selected = "World"), 
-        
-        selectInput("section.2", label = h3("Select Section"), 
-                    choices = as.list(c("World", "U.S.", "Politics", "Business", "Opinion",
-                                      "Tech", "Science", "Health", "Sports", "Arts",
-                                      "Style", "Food", "Travel")), selected = "U.S.")
-      ),
-      
-      mainPanel(
-        tabsetPanel(
-          tabPanel("TABLE", 
-                   h3("Articles"),
-                   br(),
-                   DT :: dataTableOutput("table")),
-          
-          tabPanel("Visualization",
-                   tabsetPanel(
-                     tabPanel("Monthly",
-                              br(),
-                              plotlyOutput("plot1")),
-                     tabPanel("Daily",
-                              br(),
-                              plotlyOutput("plot2")),
-                     tabPanel("Top 10 Daily",
-                              br(),
-                              plotlyOutput("plot3")), 
-                     tabPanel("Section Trend",
-                              br(),
-                              plotlyOutput("plot.trend")),
-                     tabPanel("Section Comparison Chart",
-                              br(),
-                              plotlyOutput("plot.trendline")),
-                     tabPanel("Top 10 most-talked about",
-                              br(),
-                              plotlyOutput("plot.values")))
-          )
-        )
-      )
-    )
+shinyUI (
+  navbarPage("Hidden Transistor",
+             theme = "bootstrap.css",
+             tabPanel("About" 
+             ),
+             tabPanel("Article Search",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("year.article", label = h3("Select Year"), choices = as.list(seq(1852, 2017, by = 1)), selected = 2000),
+                          selectInput("month.article", label = h3("Select Month"), 
+                                      choices = list("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+                                                     "June" = 6, "July" = 7, "August" = 8, "September" = 9, "October" = 10,
+                                                     "November" = 11, "December" = 12), selected = 1),
+                          selectInput("day.article", label = h3("Select Day"), choices = sprintf("%02d", c(seq(01,31, by = 1))), selected = 1)
+                        ),
+                        
+                        mainPanel(
+                          h3("ARTICLE SEARCH"),
+                          br(),
+                          DT :: dataTableOutput("table")
+                        )
+                      )
+             ),
+             tabPanel("Section Visualizations",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("year.viz", label = h3("Select Year"), choices = as.list(seq(1852, 2017, by = 1)), selected = 2000),
+                          selectInput("month.viz", label = h3("Select Month"), 
+                                      choices = list("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+                                                     "June" = 6, "July" = 7, "August" = 8, "September" = 9, "October" = 10,
+                                                     "November" = 11, "December" = 12), selected = 1),
+                          selectInput("day.viz", label = h3("Select Day"), choices = sprintf("%02d", c(seq(01,31, by = 1))), selected = 1),
+                          selectInput("section.viz", label = h3("Section"), choices = as.list(c("World", "U.S.", "Politics", "Business", "Opinion",
+                                                                                                          "Tech", "Science", "Health", "Sports", "Arts",
+                                                                                                          "Style", "Food", "Travel")), selected = "World")
+                        ),
+                        
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel("Monthly Section Distribution",
+                                     br(),
+                                     plotlyOutput("plot1")),
+                            tabPanel("Daily Section Distribution",
+                                     br(),
+                                     plotlyOutput("plot2")),
+                            tabPanel("Top 10 Section Daily Analysis",
+                                     br(),
+                                     plotlyOutput("plot3")),
+                            tabPanel("Section Popularity : Bubbles",
+                                     br(),
+                                     plotlyOutput("plot.trend")))
+                          
+                        )
+                      )
+             ),
+             tabPanel("Section Popularity Comparision",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("year.pop", label = h3("Select Year"), choices = as.list(seq(1852, 2017, by = 1)), selected = 2000),
+                          selectInput("month.pop", label = h3("Select Month"), 
+                                      choices = list("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+                                                     "June" = 6, "July" = 7, "August" = 8, "September" = 9, "October" = 10,
+                                                     "November" = 11, "December" = 12), selected = 1),
+                          selectInput("section1.pop", label = h3("Select Section 1"), choices = as.list(c("World", "U.S.", "Politics", "Business", "Opinion",
+                                                                                                      "Tech", "Science", "Health", "Sports", "Arts",
+                                                                                                      "Style", "Food", "Travel")), selected = "World"),
+                          selectInput("section2.pop", label = h3("Select Section 2"), choices = as.list(c("World", "U.S.", "Politics", "Business", "Opinion",
+                                                                                                      "Tech", "Science", "Health", "Sports", "Arts",
+                                                                                                      "Style", "Food", "Travel")), selected = "U.S.")
+                        ),
+                        mainPanel(
+                          h3("Section Popularity Comparision: Line"),
+                          br(),
+                          plotlyOutput("plot.trendline")))
+             ),
+             tabPanel("Popular Topics",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("year.top", label = h3("Select Year"), choices = as.list(seq(1852, 2017, by = 1)), selected = 2000),
+                          selectInput("month.top", label = h3("Select Month"), 
+                                      choices = list("January" = 1, "February" = 2, "March" = 3, "April" = 4, "May" = 5,
+                                                     "June" = 6, "July" = 7, "August" = 8, "September" = 9, "October" = 10,
+                                                     "November" = 11, "December" = 12), selected = 1)
+                        ),
+                        mainPanel(
+                          h3("10 most discussed-about topics"),
+                          br(),
+                          plotlyOutput("plot.values")
+                        )
+                      )
+             )
   )
 )
 
